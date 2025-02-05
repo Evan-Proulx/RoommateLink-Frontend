1457aadaf496e746de16e9f6c5c2b91a9931e560
eproulx <w0816747@myscc.ca>
2025-02-05 16:42:23 -0500

    Added default app theme

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ function App() {
 
   return (
     <>
-
+      <div className={"h-screen bg-[var(--color-primary)]"}>
+        <h1 className={"relative left-4 top-4 logo text-4xl"}>Roommate Link</h1>
+      </div>
     </>
   )
 }
