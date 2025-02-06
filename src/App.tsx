df49679fb6d5007cc7658c4c03d3e52edc4b3c4c
Salar Gorgees <w0831939@myscc.ca>
2025-02-05 19:26:27 -0500

    Created register file for each component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Register from "./Components/Register/Register.tsx";
 
 function App() {
 
   return (
     <>
-
+        <Register></Register>
     </>
   )
 }
---
+++ src/Components/Register/Register.tsx
@@ -0,0 +1,16 @@
+function Register() {
+
+    return (
+        <>
+            <h1 className="text-6xl text-red-400 text-left">Roommate Link</h1>
+
+            <div className="align-middle m-0">
+                <h2>Sign Up</h2>
+
+            </div>
+
+        </>
+    )
+}
+
+export default Register
