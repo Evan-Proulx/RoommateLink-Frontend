4be86f583e8ddb19925fdb5a159aa3e1ade5305b
eproulx <w0816747@myscc.ca>
2025-02-05 23:04:44 -0500

    Creted navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import './App.css'
 import Login from "./components/Login.tsx";
+import Navbar from "./components/Navbar.tsx";
 
 function App() {
 
-  return (
-    <>
-      <div className={"h-screen flex  items-center justify-center bg-[var(--color-primary)]"}>
-                <Login/>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className={"h-screen"}>
+                <Navbar/>
+                <div className={"h-full flex items-center justify-center bg-[var(--color-primary)]"}>
+                    <Login/>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
