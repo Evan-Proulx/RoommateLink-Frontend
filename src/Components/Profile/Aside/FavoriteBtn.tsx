bc89ff97cd2726f2847c4426d378863aa44c63d2
Salar Gorgees <w0831939@myscc.ca>
2025-02-10 17:58:57 -0500

    Add aside folder with component files for profile page

---
+++ src/Components/Profile/Aside/FavoriteBtn.tsx
@@ -0,0 +1,11 @@
+function FavoriteBtn() {
+
+    return (
+        <div className="flex flex-col space-y-4 flex-1 mt-2">
+
+
+        </div>
+    )
+}
+
+export default FavoriteBtn
---
+++ src/Components/Profile/Aside/Rating.tsx
@@ -0,0 +1,10 @@
+function Rating() {
+
+    return (
+        <div className="flex flex-col space-y-4 flex-1 mt-2">
+
+        </div>
+    )
+}
+
+export default Rating
