48b354548c22cca0f664e57998e1f9523b2157ef
Salar Gorgees <w0831939@myscc.ca>
2025-02-10 19:51:32 -0500

    Created files for profile page components

--- src/Components/Profile/Aside/FavoriteBtn.tsx
+++ src/Components/Profile/Aside/FavoriteBtn.tsx
@@ -2,7 +2,7 @@ function FavoriteBtn() {
     return (
         <button
             type="button"
-            className="flex items-center text-red-700 bg-white hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-6 py-3 text-center min-w-[160px] m-5 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-400 dark:focus:ring-red-800"
+            className="flex items-center text-red-700 bg-white hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-6 py-3 text-center min-w-[160px] m-5 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-300 dark:focus:ring-red-800"
         >
             <svg
                 className="w-5 h-5 text-red-700 me-2"
@@ -22,7 +22,7 @@ function FavoriteBtn() {
                 />
             </svg>
 
-            Bookmark
+            Bookmark User
         </button>
     )
 }
---
+++ src/Components/Profile/ProfilePage.tsx
@@ -0,0 +1,13 @@
+import Aside from "./Aside/Aside.tsx";
+
+function ProfilePage() {
+
+    return (
+            <div>
+                <Aside/>
+            </div>
+    )
+
+}
+
+export default ProfilePage
