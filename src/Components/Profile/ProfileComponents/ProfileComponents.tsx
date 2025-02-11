af84f6dc44bae613760aba3a067d4b53f2a3c6bc
Salar Gorgees <w0831939@myscc.ca>
2025-02-10 21:19:51 -0500

    Finished Verification badges component

--- src/Components/Profile/ProfileComponents/ProfileComponents.tsx
+++ src/Components/Profile/ProfileComponents/ProfileComponents.tsx
@@ -1,10 +1,19 @@
 import AboutSection from "./AboutSection.tsx";
+import InfoSection from "./InfoSection.tsx";
+import VerificationBadge from "./VerifictionBadge.tsx";
 
 function ProfileComponents() {
 
     return (
         <div>
-            <AboutSection/>
+            <InfoSection/>
+            <VerificationBadge
+                isIDVerified={true}
+                isPhoneVerified={true}
+                isEmailVerified={true}
+                isVideoVerified={true}
+            />
+
         </div>
     )
 
