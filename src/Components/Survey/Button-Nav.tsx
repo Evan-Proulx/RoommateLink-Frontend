61060298b24e7f343d02037ee96fcdddcc8bc654
eproulx <w0816747@myscc.ca>
2025-02-14 12:46:19 -0500

    -added survey navigation using back and next buttons

--- src/Components/Survey/Button-Nav.tsx
+++ src/Components/Survey/Button-Nav.tsx
@@ -3,10 +3,12 @@ import ShadowButton from "../Shadow-Button.tsx";
 
 const ButtonNav = () => {
     return (
-            <div className={"flex justify-between mx-5 py-8"}>
+        <div className={"fixed bottom-1 w-full hidden md:block"}>
+            <div className={"flex justify-between mx-5 p-8"}>
                 <ShadowButton value={"Back"} color={"red"}/>
                 <ShadowButton value={"Next"}/>
             </div>
+        </div>
     );
 };
 
--- src/Components/Survey/Survey.tsx
+++ src/Components/Survey/Survey.tsx
@@ -6,10 +6,12 @@ import SurveyStepper from "./SurveyStepper.tsx";
 import SurveyFormProfile from "./SurveyFormProfile.tsx";
 import SurveyFormRoommate from "./SurveyFormRoommate.tsx";
 import PropertyForm from "./PropertyForm.tsx";
+import ButtonNav from "./Button-Nav.tsx";
+import ShadowButton from "../Shadow-Button.tsx";
 
 
 const Survey = () => {
-    const [activeComponent, setActiveComponent] = useState("intro")
+    const [activeComponent, setActiveComponent] = useState("intro");
 
     const components = {
         intro: <SurveyIntro/>,
@@ -18,6 +20,24 @@ const Survey = () => {
         form3: <SurveyFormProfile/>,
         form4: <SurveyFormRoommate/>
     }
+    //All component keys. Allows for navigation between components
+    const componentKeys = ["intro", "form1", "form2", "form3", "form4"];
+
+    //Navigate to next component in list
+    const navNext = () => {
+        const currentIndex = componentKeys.indexOf(activeComponent);
+        if (currentIndex < componentKeys.length - 1) {
+            setActiveComponent(componentKeys[currentIndex + 1]);
+        }
+    }
+
+    //Navigate to previous component in list
+    const navBack = () => {
+        const currentIndex = componentKeys.indexOf(activeComponent);
+        if (currentIndex > 0) {
+            setActiveComponent(componentKeys[currentIndex - 1]);
+        }
+    }
 
     return (
         <>
@@ -25,9 +45,16 @@ const Survey = () => {
                 <h1 className={"logo"}>Roommate Link</h1>
                 <SurveyStepper setActiveComponent={setActiveComponent} activeComponent={activeComponent}/>
                 <div className={"flex items-center justify-center"}>
-                    <div className="flex flex-col items-center justify-center w-1/2 xl:w-1/3"
-                         >
+                    <div className="flex flex-col items-center justify-center w-1/2 xl:w-1/3">
                         {components[activeComponent]}
+                    </div>
+                </div>
+
+                {/*Bottom button navigation*/}
+                <div className={"fixed bottom-1 w-full hidden md:block"}>
+                    <div className={"flex justify-between mx-5 p-8"}>
+                        <ShadowButton value={"Back"} onClick={navBack} color={"red"}/>
+                        <ShadowButton value={"Next"} onClick={navNext}/>
                     </div>
                 </div>
             </div>
