8c8a0ef629f0d5a933e56c6aafbb59b05947e968
Salar Gorgees <w0831939@myscc.ca>
2025-02-17 23:14:20 -0500

    Created a new component, Survey-Map_Popup

--- src/Components/Survey/Survey-Form1.tsx
+++ src/Components/Survey/Survey-Form1.tsx
@@ -1,140 +1,135 @@
+import React from 'react';
+import ShadowButton from "../Shadow-Button.tsx";
+import ButtonNav from "./Button-Nav.tsx";
 import TimePicker from "./TimePicker.tsx";
-import {languages} from "../../Languages.ts";
-import React, {useState} from "react";
 
 const SurveyForm1 = () => {
-
-    ////////////////////////////////////////////////////////////////
-    const [city, setCity] = useState("");
-    const [hasHousing, setHasHousing] = useState(false);
-    const [budget, setBudget] = useState(1200);
-    const [school, setSchool] = useState("");
-    const [profession, setProfession] = useState("");
-    const [workingTimeFrom, setWorkingTimeFrom] = useState("");
-    const [workingTimeTo, setWorkingTimeTo] = useState("");
-    const [gender, setGender] = useState("");
-    const [language, setLanguage] = useState("");
-    const [religion, setReligion] = useState("");
-    const [diet, setDiet] = useState("");
-    ////////////////////////////////////////////////////////////////
+    const languages = [
+        {code: "en", name: "English"},
+        {code: "es", name: "Spanish"},
+        {code: "fr", name: "French"},
+        {code: "de", name: "German"},
+        {code: "zh", name: "Chinese"},
+        {code: "hi", name: "Hindi"},
+        {code: "ar", name: "Arabic"},
+        {code: "pt", name: "Portuguese"},
+        {code: "ja", name: "Japanese"},
+        {code: "ko", name: "Korean"},
+        {code: "ru", name: "Russian"},
+        {code: "it", name: "Italian"},
+        {code: "tr", name: "Turkish"},
+        {code: "nl", name: "Dutch"},
+        {code: "sv", name: "Swedish"},
+        {code: "fi", name: "Finnish"},
+        {code: "da", name: "Danish"},
+        {code: "no", name: "Norwegian"},
+        {code: "pl", name: "Polish"},
+        {code: "cs", name: "Czech"},
+        {code: "hu", name: "Hungarian"},
+        {code: "id", name: "Indonesian"},
+        {code: "th", name: "Thai"},
+    ];
 
     const diets = ["No preference", "Vegetarian", "Vegan", "Halal", "Kosher", "Pescatarian"];
 
-    //Gets times from the component and sets them
-    const handleTimeChange = (from: string, to: string) => {
-        setWorkingTimeFrom(from);
-        setWorkingTimeTo(to);
-    };
-    const getData = async (event: React.FormEvent) => {
-        event.preventDefault();
-        console.log({city, hasHousing, budget, school, profession, workingTimeFrom, workingTimeTo, gender, language, religion, diet})
-    }
     return (
-        <div>
-            <form className={"space-y-14"}>
+        <div className={"pt-24"}>
+            <form className={"space-y-18"}>
                 <h2 className={"header-text-big text-center"}>About You</h2>
 
-                <div>
-                    {/*City dropdown*/}
-                    <label htmlFor="cities" className="block mb-2 header2-text text-center">Please
-                        enter your city</label>
-                    <select id="cities"
-                            className="bg-white border-2 border-black text-gray-900 text-sm rounded-lg block w-full p-4"
-                            onChange={(event) => setCity(event.target.value)}>
-                        {/*Temporary*/}
-                        <option>Windsor</option>
-                        <option>Toronto</option>
-                    </select>
-                </div>
+                {/*City dropdown*/}
+                <label htmlFor="cities" className="block mb-2 header2-text text-center">Please
+                    enter your city</label>
+                <select id="cities"
+                        className="bg-white border-2 border-black text-gray-900 text-sm rounded-lg block w-full p-4">
+                    {/*Temporary*/}
+                    <option>Windsor</option>
+                    <option>Toronto</option>
+                </select>
 
                 {/*Housing status*/}
                 <fieldset className={""}>
                     <h2 className={"header2-text text-center"}>Current housing status</h2>
                     <div className={"flex"}>
-
                         <div className="flex items-center m-4">
-                            <input id="has-housing-true" type="radio" name="countries" value="USA"
+                            <input id="country-option-1" type="radio" name="countries" value="USA"
                                    className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600"
-                                   checked={hasHousing}
-                                   onChange={(event) => setHasHousing(true)}
-                            />
-                            <label htmlFor="has-housing-true"
+                                   checked/>
+                            <label htmlFor="country-option-1"
                                    className="block ms-2  text-sm font-medium header4-text">
                                 Already have a place
                             </label>
                         </div>
 
                         <div className="flex items-center m-4">
-                            <input id="has-housing-false" type="radio" name="countries" value="Germany"
-                                   className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600"
-                                   checked={!hasHousing}
-                                   onChange={(event) => setHasHousing(false)}
-                            />
-                            <label htmlFor="has-housing-false"
+                            <input id="country-option-2" type="radio" name="countries" value="Germany"
+                                   className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600"/>
+                            <label htmlFor="country-option-2"
                                    className="block ms-2 text-sm font-medium header4-text dark:text-gray-300">
                                 Looking for housing
                             </label>
                         </div>
-
                     </div>
                 </fieldset>
 
                 {/*Budget Slider*/}
                 <div className="relative mb-6 ">
-                    <label htmlFor="budget"
-                           className={"block mb-2 header2-text text-center"}>Budget: ${budget}</label>
-                    <input id="budget" type="range" min="100" max="10000" step="300" value={budget}
-                           className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
-                           onChange={(e) => {
-                               setBudget(+e.target.value)}}/>
+                    <label htmlFor="labels-range-input"
+                           className={"block mb-2 header2-text text-center"}>Budget</label>
+                    <input id="labels-range-input" type="range" value="1000" min="100" max="1500"
+                           className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"/>
                     <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-0 -bottom-6">$100</span>
-                    <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-1/3 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">$500</span>
-                    <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-2/3 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">$1000</span>
+                    <span
+                        className="text-sm text-gray-500 dark:text-gray-400 absolute start-1/3 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">$500</span>
+                    <span
+                        className="text-sm text-gray-500 dark:text-gray-400 absolute start-2/3 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">$1000</span>
                     <span className="text-sm text-gray-500 dark:text-gray-400 absolute end-0 -bottom-6">$10000</span>
                 </div>
 
-                <section className={"flex justify-center items-end"}>
-                    {/*Profession input*/}
-                    <div className={"flex flex-col items-center w-full"}>
-                        <label htmlFor="Profession" className="block mb-2 header2-text text-center">Profession</label>
-                        <input type="input" id="profession"
-                               className="bg-white border-2 border-black text-gray-900 text-sm rounded-lg block p-4"
-                               onChange={(event) => {
-                                   setProfession(event.target.value)
-                               }}/>
+                <div className={"flex space-x-12 pt-20"}>
+                    <div className={"flex flex-col"}>
+                        {/*Education dropdown*/}
+                        <label htmlFor="education" className="block mb-2 header2-text text-center">Education
+                            Status*</label>
+                        <select id="education"
+                                className="bg-white border-2 border-black text-gray-900 text-sm rounded-lg block w-full p-4"
+                                required>
+
+                            <option>Enrolled College/University</option>
+                            <option>Not In School</option>
+                        </select>
                     </div>
 
                     {/*School dropdown*/}
-                    <div className={"flex flex-col w-full"}>
+                    <div className={"flex flex-col"}>
                         <label htmlFor="cities" className="block mb-2 header2-text text-center">Select
                             School</label>
                         <select id="cities"
-                                className="bg-white border-2 border-black text-gray-900 text-sm rounded-lg block w-full p-4"
-                                onChange={(event) => {
-                                    setSchool(event.target.value)
-                                }}>
+                                className="bg-white border-2 border-black text-gray-900 text-sm rounded-lg block w-full p-4">
                             <option>St.Clair College</option>
                             <option>University Of Windsor</option>
                         </select>
                     </div>
-                </section>
-
-                {/*Time Picker*/}
-                <div>
-                    <h2 className={"block header2-text text-center"}>Select Working Times</h2>
-                    <TimePicker onTimeChange={handleTimeChange}/>
                 </div>
 
+                {/*Profession input*/}
+                <div className={"flex flex-col items-center"}>
+                    <label htmlFor="Profession" className="block mb-2 header2-text text-center">Profession</label>
+                    <input type="input" id="profession"
+                           className="bg-white border-2 border-black text-gray-900 text-sm rounded-lg block w-1/2 p-4"/>
+                </div>
+
+                {/*Time Picker*/}
+                <h2 className={"block mb-2 header2-text text-center"}>Select Working Times</h2>
+                <TimePicker/>
+
                 {/*Gender and language*/}
-                <section className={"flex justify-center items-end space-x-12"}>
+                <div className={"flex justify-center items-end space-x-12"}>
                     <div className={"flex flex-col w-fit"}>
                         {/*gender dropdown*/}
                         <label htmlFor="gender" className="block mb-2 header2-text text-center">Gender</label>
                         <select id="gender"
                                 className="bg-white border-2 border-black text-gray-900 text-sm rounded-lg block w-full p-4"
-                                onChange={(event) => {
-                                    setGender(event.target.value)
-                                }}
                                 required>
                             <option>Male</option>
                             <option>Female</option>
@@ -148,10 +143,7 @@ const SurveyForm1 = () => {
                         <label htmlFor="language" className="block mb-2 header2-text text-center">Select
                             language</label>
                         <select id="language"
-                                className="bg-white border-2 border-black text-gray-900 text-sm rounded-lg block w-full p-4"
-                                onChange={(event) => {
-                                    setLanguage(event.target.value)
-                                }}>
+                                className="bg-white border-2 border-black text-gray-900 text-sm rounded-lg block w-full p-4">
                             {languages.map((lang) => (
                                 <option key={lang.code} value={lang.code}>
                                     {lang.name}
@@ -159,29 +151,23 @@ const SurveyForm1 = () => {
                             ))}
                         </select>
                     </div>
-                </section>
+                </div>
 
                 {/*Religion and diet*/}
-                <section className={"flex justify-center items-end"}>
+                <div className={"flex justify-center items-end"}>
                     {/*religion input*/}
                     <div className={"flex flex-col w-full items-center"}>
                         <label htmlFor="religion" className="block header2-text text-center">Religion</label>
                         <label htmlFor="religion" className="text-end">*if applicable</label>
-                        <input type="input" id="religion" value={religion}
-                               className="bg-white border-2 border-black text-gray-900 text-sm rounded-lg block p-4"
-                               onChange={(event) => {
-                                   setReligion(event.target.value)
-                               }}/>
+                        <input type="input" id="religion"
+                               className="bg-white border-2 border-black text-gray-900 text-sm rounded-lg block p-4"/>
                     </div>
 
                     {/*diet dropdown*/}
-                    <div className={"flex flex-col w-full items-center space-x-3 md:space-x-0"}>
+                    <div className={"flex flex-col w-full items-center space-x-12"}>
                         <label htmlFor="diet" className="block mb-2 header2-text text-center">Diet preferences</label>
-                        <select id="diet" value={diet}
-                                className="bg-white border-2 border-black text-gray-900 text-sm rounded-lg block w-full p-4"
-                                onChange={(event) => {
-                                    setDiet(event.target.value)
-                                }}>
+                        <select id="diet"
+                                className="bg-white border-2 border-black text-gray-900 text-sm rounded-lg block w-full p-4">
                             {diets.map((diet) => (
                                 <option key={diet} value={diet}>
                                     {diet}
@@ -189,7 +175,8 @@ const SurveyForm1 = () => {
                             ))}
                         </select>
                     </div>
-                </section>
+                </div>
+
             </form>
         </div>
     );
