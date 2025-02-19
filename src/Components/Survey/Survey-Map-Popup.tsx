4f64cb65c2c6d4004658f45dae476eff9838eb3f
Salar Gorgees <w0831939@myscc.ca>
2025-02-18 19:33:58 -0500

    Add a button with X icon to close the popup

--- src/Components/Survey/Survey-Map-Popup.tsx
+++ src/Components/Survey/Survey-Map-Popup.tsx
@@ -24,9 +24,26 @@ const MapPopup: React.FC<MapPopupProps> = ({ isOpen, onClose, latitude, longitud
         <div className="popup-overlay fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm z-50">
             <div className="popup-content bg-white p-6 rounded-lg shadow-lg w-[80vw] max-w-lg relative z-60">
 
+                <button className="close-btn absolute top-2 right-2 text-xl font-bold" onClick={onClose}>
+
+                    <svg className="w-8 h-8 text-gray-800 dark:text-red-500"
+                         aria-hidden="true"
+                         xmlns="http://www.w3.org/2000/svg"
+                         width="24" height="24" fill="none"
+                         viewBox="0 0 24 24">
+
+                        <path stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="m15 9-6 6m0-6 6 6m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                    </svg>
+
+                </button>
+
                 <h2 className="text-center mb-4">Please select your area</h2>
                 <LoadScript googleMapsApiKey="API_KEY">
-                    <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={14}>
+                    <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={11}>
                         <Marker position={center} />
                     </GoogleMap>
                 </LoadScript>
